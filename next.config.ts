eb177fb227e378febae13cc67c4db4e2e3274058
AshishLekhyani <alekhyanisbi@gmail.com>
2026-03-29 18:03:00 +0530

    chore: configure next.config to allow github.com images in next/image

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "octodex.github.com" },
+      { protocol: "https", hostname: "em-content.zobj.net" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "github.com" },
+    ],
+  },
 };
 
 export default nextConfig;
+// images.domains: github.com added
